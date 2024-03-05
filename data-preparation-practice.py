87b17c0234dbd7b7dfe42da6114deac260923463
m-hkendall <maddy.h.kendall@gmail.com>
2024-03-05 11:58:22 -0700

    Cleaning data practice

--- data-preparation-practice.py
+++ data-preparation-practice.py
@@ -88,7 +88,37 @@ db.head()
 #print(db_population_sorted)
 
 #Visualization
-histogram = sea.histplot(db['Total'], kde=False) #kde argument, a boolean, if True, will compute a kernel density estimate to smooth the distribution and show these lines on the plot
-plt.show() #this is the command to show the chart in a new window
-kernelDensityPlot = sea.kdeplot(db['Total'], shade=True) #a kernel desntiy estimator (KDE) plot is a method for visualizing the distribution of observations in a dataset, analogous to a histogram, though the KDE represents data using a continuous probability density curve; the shade parameter adds a shade under the curve
-plt.show()
+#histogram = sea.histplot(db['Total'], kde=False) #kde argument, a boolean, if True, will compute a kernel density estimate to smooth the distribution and show these lines on the plot
+#plt.show() #this is the command to show the chart in a new window
+#kernelDensityPlot = sea.kdeplot(db['Total'], shade=True) #a kernel desntiy estimator (KDE) plot is a method for visualizing the distribution of observations in a dataset, analogous to a histogram, though the KDE represents data using a continuous probability density curve; the shade parameter adds a shade under the curve
+#plt.show()
+#lineChart = db['Total'].sort_values(ascending=False).plot() #this produces a line plot
+#plt.show()
+#barChart = db['Total'].sort_values(ascending=False).head(10).plot.bar() #the head argument returns the first 10 rows of the dataset
+#plt.show()
+#barChartHorizontal = db['Total'].sort_values(ascending=False).head(10).plot.barh()
+#plt.show()
+
+#Un/tidy data
+#What constitutes tidy data according to Hadley Wickham:
+# 1. Each variable forms a column
+# 2. Each observation forms a row
+# 3. Each type of observational unit forms a table
+print(db.head()) #this dataset has two observational units: GeographyCode, which gives the different areas of Liverpool, and the birth origin (subgroups) of an area
+#Create two different tables, one containing the Total data and one containing the subgroup data
+#Create a table "db_totals" that contains only the Total count of people for each geography code
+db_totals = db[['Total']]
+print(db_totals.head()) #db_totals is now a tidy table where each row is an observation, every variable forms a column, and there is only one observational unit in the table
+#Create a table "db_subgroups" that contains the birth origin (subgroups) columns without "Totals"
+db_subgroups = db.drop('Total', axis=1) #The drop function leaves db untouched, whereas if we had used the del function, the Totals column would have been permanently removed from db
+print(db_subgroups.head()) #db_subgroups needs further cleaning because every row is not an observation, and because every column is not a variable
+#Rearrange db_subgroups so that every row is a population subgroup in an area - we will do this by creating a pandas Series and then re-creating a data frame using the Series
+tidy_subgroups = db_subgroups.stack() #the stack method stacks the different columns into rows
+print(tidy_subgroups.head())
+tidy_subgroupsDF = tidy_subgroups.reset_index() #this creates a DataFrame from a Series
+print(tidy_subgroupsDF.head())
+tidy_subgroupsDF = tidy_subgroupsDF.rename(columns={'level_1': 'Subgroup', 0: 'Freq'}) #This renames the columns 
+print(tidy_subgroupsDF.head()) #This subgroups table is officially clean
+#Grouping, transforming, aggregating
+
+
