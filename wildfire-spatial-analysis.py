f515483c26179712aac7e8c1af3a9ec657bdc567
m-hkendall <maddy.h.kendall@gmail.com>
2024-03-13 19:23:08 -0600

    queries for 2018 wildfires and wildfires ge 200 hectares

--- wildfire-spatial-analysis.py
+++ wildfire-spatial-analysis.py
@@ -13,7 +13,24 @@ gdf_fieldname = 'SRC_AGENCY'
 alberta_wildfires_gdf = gdf[gdf[gdf_fieldname].isin(ab_fire_selector)]
 #print(alberta_wildfires_gdf.head())
 f,ax = plt.subplots(1, figsize=(12,12))
-alberta_wildfires_gdf.to_crs(epsg=3347).plot(ax=ax)
+#alberta_wildfires_gdf.to_crs(epsg=3347).plot(ax=ax)
+alberta_wildfires_gdf = alberta_wildfires_gdf.to_crs(epsg=3347)
+#print(alberta_wildfires_gdf.crs)
+alberta_wildfires_gdf.plot(ax=ax)
 cx.add_basemap(ax, crs=alberta_wildfires_gdf.crs) #check this
+ax.set_axis_off()
+#plt.show()
+
+alberta_wildfires_2018 = alberta_wildfires_gdf.query("YEAR==2018")
+#print(alberta_wildfires_2018)
+alberta_wildfires_2018.plot(ax=ax)
+ax.set_axis_off()
+cx.add_basemap(ax, crs=alberta_wildfires_2018.crs, source=cx.providers.Esri.WorldTerrain)
+#plt.show()
+
+alberta_wildfires_ge_200_ha = alberta_wildfires_gdf.query("SIZE_HA>=200")
+alberta_wildfires_ge_200_ha = alberta_wildfires_gdf.loc[alberta_wildfires_gdf['SIZE_HA'] >= 200, :] #different way of tackling the above query
+#print(alberta_wildfires_ge_200_ha)
+alberta_wildfires_ge_200_ha.plot(ax=ax)
 ax.set_axis_off()
 plt.show()
